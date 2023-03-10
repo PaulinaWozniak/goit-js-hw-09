67c24d7e072026b044f2fc404c07a38e460c091c
Paulina Woźniak <wozniak.paulina08@gmail.com>
2023-03-10 10:52:48 +0100

    homework
    correction

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -29,10 +29,12 @@ form.addEventListener('submit', event => {
 function createPromise(position, delay) {
   return new Promise ((resolve, reject) => {
     const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
       if (shouldResolve) {
         resolve({ position, delay })
       } else {
         reject ({position, delay})
       }
   }, delay);
+  });
 };
